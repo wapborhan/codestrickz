458663b4bb36b7908863648872731b4ab78ac4f8
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2025-03-08 12:05:06 +0600

    Deploy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ import withNextra from "nextra";
 const nextraConfig = withNextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  basePath: "/", // Change this based on your deployment URL
-  reactStrictMode: true,
 });
 
 export default nextraConfig;
